6ac0bd6ee4a8314025ea3d970303c2cefe893ae9
OriNachum <ori.nachum@gmail.com>
2019-02-20 03:10:08 +0200

    Set main-page to be used as main view in App.vue

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class='app'>
+    <main-page></main-page>
   </div>
 </template>
+
+<script>
+import mainPage from '@/views/MainPage.vue';
+
+export default {
+  name: 'app',
+  components: {
+    mainPage,
+  },
+};
+
+</script>
 
 <style lang="scss">
 #app {
